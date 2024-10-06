405784e9767ee277b07d3671a217c940d599c546
Nasache <magiccreator19@gmail.com>
2024-10-06 12:46:58 -0400

    Initial commit

---
+++ src/main/java/net/nathan/frights_and_foliage/FrightsAndFoliageClient.java
@@ -0,0 +1,10 @@
+package net.nathan.frights_and_foliage;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FrightsAndFoliageClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/nathan/frights_and_foliage/FrightsAndFoliageDataGenerator.java
@@ -0,0 +1,11 @@
+package net.nathan.frights_and_foliage;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FrightsAndFoliageDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
